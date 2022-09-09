f9eee5b8ae595054474ad3285d4109ebeac5a92d
adiltai <adiltay90@mail.ru>
2022-09-09 15:18:11 +0600

    add serilizers

--- movies/urls.py
+++ movies/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     # path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
     # path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
     # path("actor/<str:slug>/", views.ActorView.as_view(), name="actor_detail"),
+    path("movie/", views.MovieListView.as_view()),
+    path("movie/<int:pk>/", views.MovieDetailView.as_view())
 ]
